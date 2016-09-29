2f7c739fb8bfd6a012ecb390e3194e37868b5f7c
hb5021 <hb5021@hb5021>
2016-09-29 17:03:03 +0900

    코드랍적용

--- src/main/java/com/hanbit/team01/web/controller/ArticleController.java
+++ src/main/java/com/hanbit/team01/web/controller/ArticleController.java
@@ -1,5 +1,34 @@
 package com.hanbit.team01.web.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.hanbit.team01.core.service.ArticleService;
+
+@Controller
 public class ArticleController {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(ArticleController.class);
+
+	@Autowired
+	private ArticleService articleService;
+
+	@RequestMapping("article/index")
+	public String list(){
+
+		return "article/index";
+	}
+
+	@RequestMapping("article/index2")
+	public String list2(){
+
+		return "article/index2";
+	}
+
+
+
 
 }
---
+++ src/test/java/com/hanbit/team01/test/SecurityServiceTest.java
@@ -0,0 +1,6 @@
+package com.hanbit.team01.test;
+
+public class SecurityServiceTest {
+
+
+}
